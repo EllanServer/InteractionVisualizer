5843f399c7a4be20091a380598d8e68d8c2fc73f
Arbousier1 <elderli@foxmail.com>
2026-07-13 00:32:17 +0800

    Restore enchanting text visibility

--- common/src/main/java/com/loohp/interactionvisualizer/objectholders/EnchantmentTableAnimation.java
+++ common/src/main/java/com/loohp/interactionvisualizer/objectholders/EnchantmentTableAnimation.java
@@ -40,6 +40,7 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Display;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -319,6 +320,7 @@ public class EnchantmentTableAnimation {
     }
 
     private void setStand(DisplayEntity stand) {
+        stand.setBillboard(Display.Billboard.CENTER);
         stand.setMarker(true);
         stand.setSmall(true);
         stand.setVisible(true);
